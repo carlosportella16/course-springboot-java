be2cb93d769f80b70b41632fc293c80af84792e5
Carlos <carlosportella16@hotmail.com>
2022-02-02 16:04:57 -0300

    Project created

---
+++ src/test/java/com/udemycourse/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemycourse.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
