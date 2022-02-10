e4213fa9fbf7efeb34e57d3bbc2a91ef9a27c7ac
Carlos <carlosportella16@hotmail.com>
2022-02-10 16:45:30 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/udemycourse/course/entities/OrderItem.java
@@ -0,0 +1,83 @@
+package com.udemycourse.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.udemycourse.course.entities.pk.OrderItemPK;
+
+@Entity
+@Table(name = "td_order_item")
+public class OrderItem implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private OrderItemPK id;
+	private Integer quantity;
+	private Double price;
+
+	public OrderItem() {
+
+	}
+
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+		super();
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
---
+++ src/main/java/com/udemycourse/course/entities/pk/OrderItemPK.java
@@ -0,0 +1,59 @@
+package com.udemycourse.course.entities.pk;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.udemycourse.course.entities.Order;
+import com.udemycourse.course.entities.Product;
+
+@Embeddable
+public class OrderItemPK implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(order, product);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPK other = (OrderItemPK) obj;
+		return Objects.equals(order, other.order) && Objects.equals(product, other.product);
+	}
+
+	
+}
