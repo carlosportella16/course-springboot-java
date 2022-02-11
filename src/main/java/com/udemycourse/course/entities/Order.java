8f514bad89bc9b19a333507032cd1c71cbc272ce
Carlos <carlosportella16@hotmail.com>
2022-02-10 21:37:05 -0300

    Subtotal and Total methods

--- src/main/java/com/udemycourse/course/entities/Order.java
+++ src/main/java/com/udemycourse/course/entities/Order.java
@@ -102,6 +102,14 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
--- src/main/java/com/udemycourse/course/entities/OrderItem.java
+++ src/main/java/com/udemycourse/course/entities/OrderItem.java
@@ -64,6 +64,10 @@ public class OrderItem implements Serializable{
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public Double getSubTotal() {
+		return price * quantity;
+	}
 
 	@Override
 	public int hashCode() {
